90c97fec2f35bc9e18919fb3c12c0f6165998e76
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-25 18:36:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function firstChar(text) {
   // your code here
-	if(text == '' || text == ' ') 
-		return '';
-	// else if(text == ' ')
-	// 	return '';
-	else if(text.length > 0){
-		text = text.trim();
-		return text[0];
+	for(let i=0; i<text.length; i++){
+		if(text[i]!=" "){
+			return text[i];
+		}
 	}
+	return "";
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
-alert(firstChar(text));  
+alert(firstChar(text));
